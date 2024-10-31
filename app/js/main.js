3f0ccfafb8ffef25d7bbc03364eccf9012843f51
Ruslan <camarederic@gmail.com>
2024-10-31 21:17:26 +0300

    added the surf section

--- app/js/main.js
+++ app/js/main.js
@@ -8,9 +8,25 @@ $(function () {
     fade: true,
     asNavFor: ".slider-dots",
   });
-  $(".slider-dots").slick({
+  $(".slider-dotshead").slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: ".header__slider",
+  });
+  $(".surf-slider").slick({
+    prevArrow:
+      '<button type="button" class="slider-arrows slider-arrows__prev"><img src="icons/arrow-left.svg" alt=""/></button>',
+    nextArrow:
+      '<button type="button" class="slider-arrows slider-arrows__next"><img src="icons/arrow-right.svg" alt=""/></button>',
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: ".slider-map",
+  });
+  $(".slider-map").slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: ".surf-slider",
+    focusOnSelect: true,
   });
 });
