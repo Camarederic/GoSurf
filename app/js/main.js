b75257453ffc0326dc144ada4220e513216b93d0
Ruslan <camarederic@gmail.com>
2024-11-03 21:48:53 +0300

    added the travel section

--- app/js/main.js
+++ app/js/main.js
@@ -29,4 +29,12 @@ $(function () {
     asNavFor: ".surf-slider",
     focusOnSelect: true,
   });
+  $(".holder__slider").slick({
+    prevArrow:
+      '<button type="button" class="slider-arrows slider-arrows__prev"><img src="icons/arrow-left.svg" alt=""/></button>',
+    nextArrow:
+      '<button type="button" class="slider-arrows slider-arrows__next"><img src="icons/arrow-right.svg" alt=""/></button>',
+    infinite: true,
+    fade: true,
+  });
 });
